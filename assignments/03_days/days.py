04bb744863a99e9d1efa9b9324fcf196b182e8cf
Maxwell Chibuogwu <mochibuogwu@email.arizona.edu>
2020-02-18 05:50:24 -0700

    Finally passing all tests

--- assignments/03_days/days.py
+++ assignments/03_days/days.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
 """
-Author : maxpayyne
+Author : Maxpayyne
 Date   : 2020-02-15
 Purpose: What I do on different week days
 """
 
 import argparse
 
-
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='What to do on each day',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="What to do on each day",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('day',
-                        metavar='str',
-                        nargs='+',
-                        help='Days of the week')
+    parser.add_argument("day", metavar="str", nargs="+", help="Days of the week")
 
     return parser.parse_args()
 
@@ -30,17 +27,20 @@ def main():
 
     args = get_args()
     day = args.day
-    dict = {'Monday': 'On Mondays I never go to work',
-            'Tuesday': 'On Tuesdays I stay at home',
-            'Wednesday': 'On Wednesdays I never feel inclined',
-            'Thursday': 'On Thursdays, it’s a holiday',
-            'Friday': 'And Fridays I detest',
-            'Saturday': 'Oh, it’s much too late on a Saturday',
-            'Sunday': 'And Sunday is the day of rest'}
+    week_day = {
+        "Monday": "On Mondays I never go to work",
+        "Tuesday": "On Tuesdays I stay at home",
+        "Wednesday": "On Wednesdays I never feel inclined",
+        "Thursday": "On Thursdays, it's a holiday",
+        "Friday": "And Fridays I detest",
+        "Saturday": "Oh, it's much too late on a Saturday",
+        "Sunday": "And Sunday is the day of rest",
+    }
 
     for word in day:
-        print(dict.get(word, f'Can\'t find "{day}"'))
+        print(week_day.get(word, f'Can\'t find "{word}"'))
+
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
