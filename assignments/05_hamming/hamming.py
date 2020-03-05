8b457f145dae590d6919368bdaeceae1963d4424
Maxwell Chibuogwu <mochibuogwu@email.arizona.edu>
2020-03-04 19:05:06 -0700

    Assignment passed

--- assignments/05_hamming/hamming.py
+++ assignments/05_hamming/hamming.py
@@ -6,8 +6,6 @@ Purpose: Hamming Assignment
 """
 
 import argparse
-import os
-import sys
 
 
 # --------------------------------------------------
@@ -15,47 +13,48 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('file',
-                        help='Input file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        default=None)
+    parser.add_argument(
+        "file",
+        help="Input file",
+        metavar="FILE",
+        type=argparse.FileType("r"),
+        default=None,
+    )
 
-
-    parser.add_argument('-m',
-                        '--min',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default='0')
+    parser.add_argument(
+        "-m",
+        "--min",
+        help="A named integer argument",
+        metavar="int",
+        type=int,
+        default="0",
+    )
 
     return parser.parse_args()
 
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """This is tacky"""
 
     args = get_args()
-
-    min = args.min
 
     for line in args.file:
         word1, word = line.split()
         line_no = abs(len(word1) - len(word))
         char = list(zip(word1, word))
         dist = 0
-        for w1, w2 in char:
-                if w1 != w2: dist += 1
+        for pre, post in char:
+            if pre != post:
+                dist += 1
         tot_num = line_no + dist
 
-        if tot_num >= min:
-            print(f'{tot_num:8}:{word1:20}{word:20}')
+        if tot_num >= args.min:
+            print(f"{tot_num:8}:{word1:20}{word:20}")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
