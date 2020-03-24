d5b99ca1e0c21b74d39b34abe9edefb866ed16a2
Maxwell Chibuogwu <mochibuogwu@email.arizona.edu>
2020-03-23 18:00:37 -0700

    Add

--- assignments/07_rna/transcribe.py
+++ assignments/07_rna/transcribe.py
@@ -7,32 +7,36 @@ Purpose: Maxpayyne is Programming
 
 import argparse
 import os
-import sys
-
 
 # --------------------------------------------------
 def get_args():
     """Argparse: Getting command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Transcribing DNA into RNA',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('File',
-                        help='Input file(s)',
-                        metavar='FILE',
-                        nargs='+',
-                        type=argparse.FileType('r'),
+        description="Transcribing DNA into RNA",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument('-o',
-                        '--outdir',
-                        help='Output directory', type=str,#argparse.FileType('wt'),
-                        metavar='DIR', default='out',)
+
+    parser.add_argument(
+        "File",
+        help="Input file(s)",
+        metavar="FILE",
+        nargs="+",
+        type=argparse.FileType("r"),
+    )
+    parser.add_argument(
+        "-o",
+        "--outdir",
+        help="Output directory",
+        type=str,  # argparse.FileType('wt'),
+        metavar="DIR",
+        default="out",
+    )
 
     args = parser.parse_args()
 
     if not os.path.isdir(args.outdir):
-        os.makedirs(outdir)
+        os.makedirs(args.outdir)
 
     return parser.parse_args()
 
@@ -42,35 +46,34 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    base = os.path.basename(args.File)
+    base = os.path.basename('./inputs/input1.txt')
     out_dir = args.outdir
-    os.path.join(out_dir, base)
+
     num_files = 0
     for fh in args.File:
         num_files += 1
         out_file = os.path.join(out_dir, os.path.basename(fh.name))
-        out_fh = open(out_file, 'wt')
-        num_lines = 0       # base = os.path.basename(fh.name)
+        out_fh = open(out_file, "wt")
+        num_lines = 0  # base = os.path.basename(fh.name)
         for line in fh:
             num_lines += 1
-            line = line.replace('T', 'U').strip()
-            #print(f'{num_lines} and {fh.name}')
+            line.strip().replace("T", "U")
+            out_fh.write(line)  # or print(line, file=out_fh)
+        out_fh.close()
     if num_lines == 1:
-        sequence = 'sequence'
+        sequence = "sequence"
     else:
-        sequence = 'sequences'
+        sequence = "sequences"
     if num_files == 1:
-        files = 'file'
+        files = "file"
     else:
-        files = 'files'
-    # out_file = os.path.join(outdir, os.path.basename(fh.name))
-    # out_fh = open(out_file, wt)
-    # out_fh.write(line) #or print(line, file=out_fh)
-    # out_fh.close()
-    # print(os.path.isdir(outdir))
-    print(f'Done, wrote {num_lines} {sequence} in {num_files} {files} to directory \"{args.outdir}\".')
+        files = "files"
+
+    print(
+        f'Done, wrote {num_lines} {sequence} in {num_files} {files} to directory "{args.outdir}".'
+    )
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
