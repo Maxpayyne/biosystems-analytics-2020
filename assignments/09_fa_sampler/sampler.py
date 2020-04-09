0ebf553ac7b9ad70e7af8f5d7751e65e19b1c03e
Maxwell Chibuogwu <mochibuogwu@email.arizona.edu>
2020-04-08 19:29:35 -0700

    Almost done

--- assignments/09_fa_sampler/sampler.py
+++ assignments/09_fa_sampler/sampler.py
@@ -11,41 +11,44 @@ import random
 from Bio import SeqIO
 
 
-
 # --------------------------------------------------
 def get_args():
     """Argparse: Getting command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Probabalistically subset FASTA files',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Probabalistically subset FASTA files",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('text',
-                        metavar='FILE',
-                        nargs='+',
-                        type=argparse.FileType('r'),
-                        default='',
-                        help='Input FASTA file(s)')
+    parser.add_argument(
+        "text",
+        metavar="FILE",
+        nargs="+",
+        type=argparse.FileType("r"),
+        default="",
+        help="Input FASTA file(s)",
+    )
 
-    parser.add_argument('-p',
-                        '--pct',
-                        help='Percent of reads',
-                        metavar='reads',
-                        type=float,
-                        default=0.1)
-    parser.add_argument('-s',
-                        '--seed',
-                        metavar='seed',
-                        type=int,
-                        help='Random seed value',
-                        default=None,)
+    parser.add_argument(
+        "-p", "--pct", help="Percent of reads", metavar="reads", type=float, default=0.1
+    )
+    parser.add_argument(
+        "-s",
+        "--seed",
+        metavar="seed",
+        type=int,
+        help="Random seed value",
+        default=None,
+    )
 
-    parser.add_argument('-o',
-                        '--outdir',
-                        help='Output directory',
-                        metavar='DIR',
-                        type=str,
-                        default='out')
+    parser.add_argument(
+        "-o",
+        "--outdir",
+        help="Output directory",
+        metavar="DIR",
+        type=str,
+        default="out",
+    )
     args = parser.parse_args()
 
     if not 0 <= args.pct <= 1:
@@ -69,20 +72,22 @@ def main():
     for i, fh in enumerate(args.text, start=1):
         basename = os.path.basename(fh.name)
         out_file = os.path.join(args.outdir, basename)
-        print(f'{i:3}: {fh.name}')
+        print(f"{i:3}: {fh.name}")
 
-        out_fh = open(out_file, 'wt')
-        for rec in SeqIO.parse(fh, 'fasta'):
+        out_fh = open(out_file, "wt")
+        for rec in SeqIO.parse(fh, "fasta"):
             if random.random() == args.pct:
-                SeqIO.write(rec, out_fh, 'fasta')
+                SeqIO.write(rec, out_fh, "fasta")
                 # print(rec)
 
         out_fh.close()
-    print(f'Wrote {len(rec)} sequence{"" if len(rec) == 1 else "s"} '
-          f'from {i} file{"" if i == 1 else "s"} to '
-          f'directory "{args.outdir}"')
+    print(
+        f'Wrote {len(rec)} sequence{"" if len(rec) == 1 else "s"} '
+        f'from {i} file{"" if i == 1 else "s"} to '
+        f'directory "{args.outdir}"'
+    )
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
